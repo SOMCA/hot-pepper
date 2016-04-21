01e946e0d4f488c0edc088f326a4947bed6de772
Antonin Carette <antonin.carette@gmail.com>
2016-04-21 13:40:54 -0400

    Debugging

--- software/powertool/powertool.py
+++ software/powertool/powertool.py
@@ -1,15 +1,15 @@
 import argparse
 
-from time import sleep
-
-from yoctoammeter import YoctoDevice
+from cstatistics import CStatistics
 from csv_export import CSVExport
+from time import sleep
+from yoctoammeter import YoctoDevice
 
 def main():
 
     #Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--framerate", type=str, default="50/s"
+    parser.add_argument("-f", "--framerate", type=str, default="50/s",
                         help="Give to the main program the framerate.")
     parser.add_argument("-o", "--output", type=str,
                         help="Give an output file name to store measured values.")
@@ -17,21 +17,22 @@ def main():
                         help="Ask to output basic statistics (mean, median, pvariance,...) on measured values.")
     parser.add_argument("-t", "--time", type=int, default=60,
                         help="Due time to measure data.")
-    parser.parse_args()
+    args = parser.parse_args()
 
     #Get the yocto device
-    yocto_device = YoctoDevice(parser.framerate)
+    yocto_device = YoctoDevice(args.framerate)
     print(yocto_device)
 
     yocto_device.run()
-    sleep(parser.time)
+    sleep(args.time)
     yocto_device.stopMeasure()
 
-    if parser.output:
-        csv_obj = CSVExport(parser.output)
+    if args.output:
+        CSVExport(args.output).export_data(yocto_device._values)
 
-    if parser.statistics:
-        csv_obj.export_data(yocto_device._values)
+    if args.statistics:
+        measures = [y for x, y in yocto_device._values]
+        print(CStatistics(measures))
 
 if __name__ == '__main__':
     main()
