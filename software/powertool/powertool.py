cf330069af7cb900d47aac81bbd6997a33d73dba
Antonin Carette <antonin.carette@gmail.com>
2016-04-21 13:24:52 -0400

    Enhancements: Many feature have been added to the main function

--- software/powertool/powertool.py
+++ software/powertool/powertool.py
@@ -7,22 +7,31 @@ from csv_export import CSVExport
 
 def main():
 
-    args = argparse
+    #Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--framerate", type=str, default="50/s"
+                        help="Give to the main program the framerate.")
+    parser.add_argument("-o", "--output", type=str,
+                        help="Give an output file name to store measured values.")
+    parser.add_argument("-s", "--statistics", action="store_true",
+                        help="Ask to output basic statistics (mean, median, pvariance,...) on measured values.")
+    parser.add_argument("-t", "--time", type=int, default=60,
+                        help="Due time to measure data.")
+    parser.parse_args()
 
-    yocto_device = YoctoDevice()
-
+    #Get the yocto device
+    yocto_device = YoctoDevice(parser.framerate)
     print(yocto_device)
 
     yocto_device.run()
-
-    sleep(5)
-
+    sleep(parser.time)
     yocto_device.stopMeasure()
 
-    csv_obj = CSVExport("mytest.csv")
+    if parser.output:
+        csv_obj = CSVExport(parser.output)
 
-    csv_obj.export_data(yocto_device._values)
-
+    if parser.statistics:
+        csv_obj.export_data(yocto_device._values)
 
 if __name__ == '__main__':
     main()
