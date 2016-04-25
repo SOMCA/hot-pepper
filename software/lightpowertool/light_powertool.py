d4bd4464a89214cb35475019d9dfaec4bf5deba7
k0pernicus <antonin.carette@gmail.com>
2016-04-25 10:53:51 -0400

    style: Update style

--- software/lightpowertool/light_powertool.py
+++ software/lightpowertool/light_powertool.py
@@ -5,23 +5,32 @@ from classes.csv_export import CSVExport
 from time import sleep
 from classes.yoctoammeter import YoctoDevice
 
-def main():
 
-    #Parse arguments
+def main():
+    r"""
+    Main program of LightPowertool.
+
+    LightPowertool is a software to measure automatically the energy\
+     consumption of devices, using the Yoctopuce ammeter.
+    """
+    # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--framerate", type=str, default="50/s",
                         help="Give to the main program the framerate.")
     parser.add_argument("-n", "--network", action="store_true",
-                        help="Use the network to send your data - local server like LightPowertoolServer")
+                        help="Use the network to send your data\
+                        - local server like LightPowertoolServer")
     parser.add_argument("-o", "--output", type=str,
-                        help="Give an output file name to store measured values.")
+                        help="Give an output file name\
+                         to store measured values.")
     parser.add_argument("-s", "--statistics", action="store_true",
-                        help="Ask to output basic statistics (mean, median, pvariance,...) on measured values.")
+                        help="Ask to output basic statistics\
+                         (mean, median, pvariance,...) on measured values.")
     parser.add_argument("-t", "--time", type=int, default=60,
                         help="Due time to measure data.")
     args = parser.parse_args()
 
-    #Get the yocto device
+    # Get the yocto device
     yocto_device = YoctoDevice(args.framerate, network=args.network)
     print(yocto_device)
 
