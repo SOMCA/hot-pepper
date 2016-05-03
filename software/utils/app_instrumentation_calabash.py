827690dde9994e2aca11f89ffde468bc3a867264
Antonin Carette <antonin.carette@gmail.com>
2016-05-03 11:16:28 -0400

    Enhancement: First version of the Calabash instrumentation script refactored

--- software/utils/app_instrumentation_calabash.py
+++ software/utils/app_instrumentation_calabash.py
@@ -6,6 +6,21 @@ import time
 
 from subprocess import call
 from datetime import datetime
+from yoctoammeter import YoctoDevice
+
+gc_app = "PAPRIKA_gccall.apk"
+
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Get stats from Powertool output')
+	parser.add_argument('-a', '--app', type=str, required=True, help="the package name of the application to study")
+	parser.add_argument('-b', '--begin', type=int, default=1, help="The beginning to run tests")
+	parser.add_argument('-f', '--feature', type=str, default=None, help="Specify the feature to run")
+	parser.add_argument('-n', '--nbr', type=int, default=31, help="Number of tests to run")
+	parser.add_argument('--gc', action="store_true", help="Launch a phone application to ask to Android to run the garbage collector - before each test")
+	parser.add_argument('-o', '--output', type=str, default=None, required=True, help="Specify output path")
+	parser.add_argument('-r', '--refactored', type=str, default=None, help="Specify if the application is refactored")
+	parser.add_argument('-s', '--scenario', type=str, default=None, help="Specify the scenario to run")
+	return parser.parse_args()
 
 def threaded_test(stop_event, nb, app, feature=None, scenario=None):
 	"""
@@ -46,67 +61,78 @@ def threaded_test(stop_event, nb, app, feature=None, scenario=None):
 	f.write("\n****** STOP : "+str(datetime.now()) + "\n")
 	f.flush()
 
-def launch_powertool(stop_event, output_path):
-	"""
-		To launch Powertool for each instance of threaded_test
-	"""
-	p = subprocess.Popen(["powertool", "-d", "yocto", "-o", output_path], shell=False)
+def launch_gc_app():
+	# The application "PAPRIKA_gccall.apk" must be installed on the Android smartphone!
+	gc_call = subprocess.Popen(["adb","shell","am","start","-n","paprika.io.gccall/.MainActivity"], shell=False)
+	gc_call.wait()
+
+def clear_cache_file(app):
+	# Clear cache files at the ending
+	call(["adb","shell","pm","clear",app])
+
+def force_stop_app(app):
+	# Force the application which launch the garbage collector to stop
+	p = subprocess.Popen(["adb","shell","am","force-stop", app], shell=False)
 	p.wait()
-	stop_event.set()
+
+def get_new_output(output, app, refactored, instance):
+	# Output name for non-refactored application
+	return "/".join([output, "/{0}_test_{1}.csv".format(app.split("/")[-1], instance)]) if not refactored
+		   else return "/".join([output, "/{0}_test_R{1}_{2}.csv".format(app.split("/")[-1], refactored, instance)])
 
 def main():
 
-	parser = argparse.ArgumentParser(description='Get stats from Powertool output')
-	parser.add_argument('-a', '--app', type=str, required=True, help="the package name of the application to study")
-	parser.add_argument('-b', '--begin', type=int, default=1, help="The beginning to run tests")
-	parser.add_argument('-f', '--feature', type=str, default=None, help="Specify the feature to run")
-	parser.add_argument('-n', '--nbr', type=int, default=31, help="Number of tests to run")
-	parser.add_argument('--gc', action="store_true", help="Launch a phone application to ask to Android to run the garbage collector - before each test")
-	parser.add_argument('-o', '--output', type=str, default=None, required=True, help="Specify output path")
-	parser.add_argument('-r', '--refactored', type=str, default=None, help="Specify if the application is refactored")
-	parser.add_argument('-s', '--scenario', type=str, default=None, help="Specify the scenario to run")
-	args = parser.parse_args()
+	args = parse_arguments()
 
 	if args.scenario and not (args.scenario[0] == "@"):
-		print("The scenario must have the '@' sign!")
-		sys.exit()
+		print("/!\ [WARNING] /!\ \n===> The scenario must have the '@' sign!")
+		sys.exit(1)
 
 	for x in range(args.begin, args.begin + args.nbr):
 		"""
 			Test nbr times the app and the energy consumption
 		"""
 
-		current_output = args.output
+		# Output name to store data
+		current_output = get_new_output(args.output, args.app, args.refactored, x)
 
-		if not args.refactored:
-			current_output += "/{0}_test_{1}.csv".format(args.app.split("/")[-1], x)
-		else:
-			current_output += "/{0}_test_R{1}_{2}.csv".format(args.app, args.refactored, x)
+		force_stop_app(args.app)
 
-		p = subprocess.Popen(["adb","shell","am","force-stop",args.app], shell=False)
-		p.wait()
-
+		# Call the garbage collector to launch
 		if args.gc:
-			# The application "PAPRIKA_gccall.apk" must be installed on the Android smartphone!
-			gc_call = subprocess.Popen(["adb","shell","am","start","-n","paprika.io.gccall/.MainActivity"], shell=False)
-			gc_call.wait()
+			launch_gc_app()
+			force_stop_app(gc_app)
 
-		time.sleep(5)
+		# Wait 10 seconds to avoid biases caused with the garbage collector application launch
+		time.sleep(10)
 
-		thread_stop= threading.Event()
+		yocto_device = None
 
+		try:
+			yocto_device = YoctoDevice("75/s", True)
+			print(yocto_device)
+		except Exception as yocto_exception:
+			print(yocto_exception)
+			sys.exit(1)
+
+		# Create an event to stop the current thread
+		thread_stop = threading.Event()
+
+		# Launch the test
 		ttest = threading.Thread(target = threaded_test, args=(thread_stop,x,args.app,args.feature,args.scenario))
-		tpower = threading.Thread(target = launch_powertool, args=(thread_stop,current_output))
 		ttest.start()
-		tpower.start()
 
+		# Run the device
+		yocto_device.run()
+
+		# Wait tests stops...
 		ttest.join()
-		tpower.join()
+		# ... and stop the yocto device!
+		yocto_device.stopMeasure()
 
 	print("DONE")
 
-	# Clear cache files at the ending
-	call(["adb","shell","pm","clear",args.app])
+	clear_cache_file(app)
 
 if __name__ == '__main__':
 	main()
