d35e4de4a1a6c30f7f0d2ea2759e9d73d64976af
Antonin Carette <antonin.carette@gmail.com>
2016-08-15 15:11:32 -0400

    Add 2 scripts (examples)

---
+++ LAUNCH_CALCULATOR_ALL.sh
@@ -0,0 +1,1 @@
+cd $SCENARIOS/app_calculator/ && python3.5 $SOFTWARE/app_instrumentation_calabash.py -a $APPS/CALCULATOR/Calculator_NOALL.apk -o $MESURES -r RALL -n 20
---
+++ LAUNCH_SERVER.sh
@@ -0,0 +1,1 @@
+python3.5 $SOFTWARE/light_powertool_server.py -s 7200
